744ee12616591a76e138c28537d1187ff3d02fc2
parsel95 <venger090239@gmail.com>
2023-09-12 10:52:00 +0200

    частичное добавление js

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Рабочий код, отвечающий за поповер(корзина)
+
+let popoverCarts = document.querySelectorAll('.popover-cart');
+let popoverOpenButtons = document.querySelectorAll('.popover__open-button');
+let popoverCountIcon = document.querySelector('.navigation-menu__icon');
+let popoverTextCount = document.querySelector('.popover__text--count');
+let popoverTextSum = document.querySelector('.popover__text--sum');
+let popoverCount = 2;
+let popoverSum = 2000;
+
+for (let popoverOpenButton of popoverOpenButtons) {
+  popoverOpenButton.onclick = function () {
+    for (let popoverCart of popoverCarts) {
+      popoverCart.classList.toggle('popover__active');
+    }
+  }
+}
+
+let popoverDeleteButtons = document.querySelectorAll('.popover__product-delete-button');
+
+for (let popoverDeleteButton of popoverDeleteButtons) {
+  popoverDeleteButton.onclick = function () {
+    popoverDeleteButton.parentElement.remove();
+    popoverCount = popoverCount - 1;
+    popoverSum -= 1000;
+    popoverCountIcon.textContent = popoverCount;
+    popoverTextCount.textContent = 'Товаров: ' + popoverCount;
+    popoverTextSum.textContent = 'Сумма: ' + popoverSum + ' ₽';
+  }
+}
+
+
+
+
+
